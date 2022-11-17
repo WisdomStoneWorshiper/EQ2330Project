14af654f378cd2833d37e7be49f826c14fe0c499
Johnny Lo <klloaj@connect.ust.hk>
2022-11-17 17:56:13 +0100

    3.2 done

--- EQ2330_pj1/matlab/q3.m
+++ EQ2330_pj1/matlab/q3.m
@@ -4,14 +4,32 @@ r = 8; % from the instruction
 h = myblurgen('gaussian', r);
 g = conv2(f, h, 'same');
 [x_max, y_max] = size(g);
+
 for i = 1:x_max
+
     for j = 1:y_max
-        g(i,j) = min([max([g(i,j),0]), 255]);
+        g(i, j) = min([max([g(i, j), 0]), 255]);
     end
+
 end
 
+f = im2double(f);
+g = im2double(g);
 
-imagesc(f);
+F = fft2(f);
+F = fftshift(F);
+F_spectra = log(abs(F));
+
+G = fft2(g);
+G = fftshift(G);
+G_spectra = log(abs(G));
+
+imagesc((F_spectra));
 colormap gray(256);
-figure, imagesc(g);
+figure, imagesc(G_spectra);
 colormap gray(256);
+
+% imagesc(f);
+% colormap gray(256);
+% figure, imagesc(g);
+% colormap gray(256);
