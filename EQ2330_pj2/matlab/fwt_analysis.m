03668d6e516dc0afbd37799d5c86a5c7069231a7
Johnny Lo <klloaj@connect.ust.hk>
2022-11-28 00:13:39 +0100

    much looks like the matlab library dwt, just need to shift back after done

--- EQ2330_pj2/matlab/fwt_analysis.m
+++ EQ2330_pj2/matlab/fwt_analysis.m
@@ -1,13 +1,15 @@
 function [cA,cD] = fwt_analysis(signal, LPF)
     HPF = wrev(qmf(wrev(LPF)));
     
-    padded = padarray(signal, [0,length(LPF)-1], "circular", "pre");
+    padded = padarray(signal, [0,length(LPF)-1], "circular");
     
-    output_lp = conv(padded, LPF, "valid");
-    output_hp = conv(padded, HPF, "valid");
+    output_lp = conv(padded, LPF, "full");
+    output_hp = conv(padded, HPF, "full");
     
+    output_lp = output_lp(length(LPF)+1:length(LPF)+length(signal)-1);
+    output_hp = output_hp(length(LPF)+1:length(LPF)+length(signal)-1);
     cA = downsample(output_lp, 2);
     cD = downsample(output_hp, 2);
-
+    
 end
 
