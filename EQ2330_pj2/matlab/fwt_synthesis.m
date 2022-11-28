49e94b86b04c0f42c0f737f613bb3e4256f71d45
Johnny Lo <klloaj@connect.ust.hk>
2022-11-28 11:53:09 +0100

    3.1 really done, result same as matlab function

--- EQ2330_pj2/matlab/fwt_synthesis.m
+++ EQ2330_pj2/matlab/fwt_synthesis.m
@@ -5,12 +5,14 @@ function output = fwt_synthesis(cA, cD, LPF)
     upsampled_cD = upsample(cD ,2);
 
     padded_cA = padarray(upsampled_cA, [0,length(LPF)-1], "circular");
-    padded_cD = padarray(upsampled_cD, [0,length(LPF)-1], "circular");
+    padded_cD = padarray(upsampled_cD, [0,length(HPF)-1], "circular");
 
     output1 = conv(padded_cA, LPF, "full");
     output2 = conv(padded_cD, HPF, "full");
 
+    output1 = output1(length(LPF):end - (length(LPF)-1)*2);
+    output2 = output2(length(HPF):end - (length(HPF)-1)*2);
+
     output = output1 + output2;
-%     output = output(length(LPF):end-length(LPF)+1);
 end
 
