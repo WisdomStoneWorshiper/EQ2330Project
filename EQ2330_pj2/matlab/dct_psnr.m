b6cc21a61357ce58ec3a1d80e1ce459829011167
Angel Chiu <72505096+LokYingCHIU@users.noreply.github.com>
2022-12-06 15:26:27 +0100

    Update dct_psnr.m

--- EQ2330_pj2/matlab/dct_psnr.m
+++ EQ2330_pj2/matlab/dct_psnr.m
@@ -9,13 +9,13 @@ mse_q = 0;
 for i = 1:size_img(1)/M
     for j = 1:size_img(2)/M
         img_block = img((i-1)*M+1:M*i,(j-1)*M+1:M*j); % index of the row: (i-1)*M+1:M*i, index of the column: (j-1)*M+1:M*j
-        [img_dct, A] = mydct2(img_block, M); % DCT
+        img_dct = dct2(img_block); % DCT
+        A = dct2matrixA(M); % get the matrix A to reconstruct the image
         img_q = quantizer(img_dct, 1); % quantizer
         mse_q = mse_q + sum((img_dct(:)-img_q(:)).^2)/length(img_dct(:));
-        img_rc((i-1)*M+1:M*i,(j-1)*M+1:M*j) = A'*img_q*A; % reconstructed (IDCT)
+        img_rc((i-1)*M+1:M*i,(j-1)*M+1:M*j) = A'*img_q*A; % reconstructed image
     end
 end
 
 mse_q = mse_q/(size_img(1)/M*size_img(2)/M); % the mse between original and the quantized dct coeffs
-d = sum((img(:)-img_rc(:)).^2)/length(img(:)); % d = the mse between original and the reconstructed image
-psnr = 10*log10(255^2/d); % psnr for 8-bit images
+d = mse(img, img_rc); % d = the mse between original and the reconstructed image
