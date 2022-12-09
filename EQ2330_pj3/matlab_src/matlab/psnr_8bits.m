837d4cd5f7fc25245279b9830aff4f0b3151c823
Johnny Lo <klloaj@connect.ust.hk>
2022-12-09 16:54:55 +0100

    changed q1

---
+++ EQ2330_pj3/matlab_src/matlab/psnr_8bits.m
@@ -0,0 +1,3 @@
+function output = psnr_8bits(img1,img2)
+    output = 10 * log10((255^2)/mse(img1,img2));
+end
--- EQ2330_pj3/matlab_src/matlab/q1.m
+++ EQ2330_pj3/matlab_src/matlab/q1.m
@@ -1,18 +1,72 @@
-foreman = yuv_import_y('../../foreman_qcif/foreman_qcif.yuv',[176,144],50);
+clc;
+clear;
+
+frame_width = 176;
+frame_height = 144;
+block_size = 16;
+num_of_frames = 50;
+
+foreman = yuv_import_y('../../foreman_qcif/foreman_qcif.yuv', [frame_width, frame_height], num_of_frames);
+
+foreman_dct = cell(num_of_frames ,1);
+foreman_quantized = cell(4, num_of_frames);
+foreman_idct = cell(4, num_of_frames);
+foreman_psnr = zeros(4, num_of_frames);
+%dct to all frames
+
+for i = 1:num_of_frames
+    foreman_dct{i,1} = blockproc(foreman{i,1}, [8 8], @(block_struct) dct2(block_struct.data));
+    for j = 3:6
+        foreman_quantized{j-2,i} = quantizer(foreman_dct{i,1},2^j);
+        foreman_idct{j-2,i} = blockproc(foreman_quantized{j-2,i}, [8 8], @(block_struct) idct2(block_struct.data));
+        foreman_psnr(j-2,i) = psnr_8bits(foreman{i,1}, foreman_idct{j-2,i});
+    end
+end
+
+avg_foreman_psnr = mean(foreman_psnr,2);
+
+bitrates = zeros(4,frame_height/block_size,frame_width/block_size);
+
+% for i = 3:6
+%     concated_coffs = cell(frame_height/block_size,frame_width/block_size);
+%     for j = 1:num_of_frames
+%         divided = mat2cell(foreman_quantized{i-2,j},repelem(16,frame_height/block_size),repelem(16,frame_width/block_size)); 
+%         for k = 1:frame_height/block_size
+%             for l= 1:frame_width/block_size
+%                 if j ==1
+%                     concated_coffs{k,l}=divided{k,l}(:);
+%                 else
+%                     concated_coffs{k,l} = [concated_coffs{k,l};divided{k,l}(:)];
+%                 end
+%                 if j == num_of_frames
+%                     bitrates(i,k,l)=bitrate(concated_coffs{k,l});
+%                 end
+%             end
+%         end 
+%     end
+% end
+
+
+
 foreman16x16 = cell([50 1]);
 
 %subdivide frames into 16x16 blocks%
 for i = 1:length(foreman)
-    foreman16x16{i} = mat2cell(foreman{i},[16 16 16 16 16 16 16 16 16],[16 16 16 16 16 16 16 16 16 16 16]);
+    foreman16x16{i} = mat2cell(foreman{i}, [16 16 16 16 16 16 16 16 16], [16 16 16 16 16 16 16 16 16 16 16]);
 end
 
 foreman8x8 = foreman16x16;
+
 for i = 1:length(foreman16x16)
-    for j = 1:9
+
+    for i = 1:9
+
         for k = 1:11
-            foreman8x8{i,1}{j,k} = mat2cell(foreman16x16{i,1}{j,k},[8 8],[8 8]);
+            foreman8x8{i, 1}{i, k} = mat2cell(foreman16x16{i, 1}{i, k}, [8 8], [8 8]);
         end
+
     end
+
 end
 
 foreman8x8_dct = foreman8x8;
@@ -20,27 +74,27 @@ foreman8x8_dctquant3 = foreman8x8;
 foreman8x8_dctquant4 = foreman8x8;
 foreman8x8_dctquant5 = foreman8x8;
 foreman8x8_dctquant6 = foreman8x8;
+
 for i = 1:length(foreman16x16)
-    for j = 1:9
+
+    for i = 1:9
+
         for k = 1:11
+
             for a = 1:2
+
                 for b = 1:2
-                    foreman8x8_dct{i}{j,k}{a,b} = dct2(foreman8x8{i}{j,k}{a,b});
-                    foreman8x8_dctquant3{i}{j,k}{a,b} = quantizer(foreman8x8{i}{j,k}{a,b},3);
-                    foreman8x8_dctquant4{i}{j,k}{a,b} = quantizer(foreman8x8{i}{j,k}{a,b},4);
-                    foreman8x8_dctquant5{i}{j,k}{a,b} = quantizer(foreman8x8{i}{j,k}{a,b},5);
-                    foreman8x8_dctquant6{i}{j,k}{a,b} = quantizer(foreman8x8{i}{j,k}{a,b},6);
+                    foreman8x8_dct{i}{i, k}{a, b} = dct2(foreman8x8{i}{i, k}{a, b});
+                    foreman8x8_dctquant3{i}{i, k}{a, b} = quantizer(foreman8x8{i}{i, k}{a, b}, 2 ^ 3);
+                    foreman8x8_dctquant4{i}{i, k}{a, b} = quantizer(foreman8x8{i}{i, k}{a, b}, 2 ^ 4);
+                    foreman8x8_dctquant5{i}{i, k}{a, b} = quantizer(foreman8x8{i}{i, k}{a, b}, 2 ^ 5);
+                    foreman8x8_dctquant6{i}{i, k}{a, b} = quantizer(foreman8x8{i}{i, k}{a, b}, 2 ^ 6);
                 end
+
             end
+
         end
+
     end
+
 end
-
-            
-
-
-
-
-
-
-
